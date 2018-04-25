f4e8f5d03a91de9e1934f8a219094997e725ee1d
Thomas Dexter <tsdexter@gmail.com>
2018-04-25 13:58:04 -0400

    extras 💅
    css and file loading working

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
+// import css
+import "../css/style.css";
+
+// import a file
+import logo from "../assets/react.png";
+
 export default class Hello extends Component {
   render() {
-    return <div>Hello from react es6</div>;
+    return (
+      <div>
+        Hello from react es6
+        <img src={logo} alt="React" />
+      </div>
+    );
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const paths = {
   JS: path.resolve(__dirname, "src/js")
 };
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 // Webpack configuration
 module.exports = {
   entry: path.join(paths.JS, "app.js"),
@@ -17,7 +18,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(paths.SRC, "index.html")
-    })
+    }),
+    new ExtractTextPlugin("style.bundle.css") // CSS will be extracted to this bundle file
   ],
   // Loaders configuration -> ADDED IN THIS STEP
   // We are telling webpack to use "babel-loader" for .js and .jsx files
@@ -27,6 +29,21 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"]
+      },
+      // CSS loader to CSS files
+      // Files will get handled by css loader and then passed to the extract text plugin
+      // which will write it to the file we defined above
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: "css-loader"
+        })
+      },
+      // File loader for image assets -> ADDED IN THIS STEP
+      // We'll add only image extensions, but you can things like svgs, fonts and videos
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: ["file-loader"]
       }
     ]
   },
